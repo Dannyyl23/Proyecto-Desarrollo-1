7881513d73cd4a9345ba787ecf99b575d30deabc
Julián Potes Villada <julianpotesvillada@gmail.com>
2024-11-23 18:05:29 -0500

    Creacion clase boleto
    Se crea la clase boleto, pendiente por añadir logica

---
+++ Boleto.java
@@ -0,0 +1,20 @@
+public class Boleto {
+    private int cantidadBoletos;
+    private int valorBoleto;
+    private int totalBoletos;
+    private String fechaDeVenta;
+
+    public void comprarBoleto() {
+        // Implementación
+    }
+
+    public void seleccionarAsiento() {
+        // Implementación
+    }
+
+    public void verDisponibilidad() {
+        // Implementación
+    }
+}
+
+
