5aed1619df6308069515d54c31cb911ea6d4904e
ErikaAvilaGarzon <erikaavilagarzon@gmail.com>
2024-11-23 17:40:38 -0500

    Clase Pelicula
    Creación de la estructura para la clase película, pendiente de desarrollo de lógica.

---
+++ Pelicula.java
@@ -0,0 +1,22 @@
+public class Pelicula {
+    private String titulo;
+    private String duracion;
+    private int clasificacion;
+    private String genero;
+
+    public void crearPelicula() {
+        // Implementación
+    }
+
+    public void editarPelicula() {
+        // Implementación
+    }
+
+    public void eliminarPelicula() {
+        // Implementación
+    }
+
+    public void definirHorario() {
+        // Implementación
+    }
+}
