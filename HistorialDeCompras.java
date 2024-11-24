d391d1c2c86e1a77d5d6683a94818b550212744c
SebastianRojas007 <166966005+SebastianRojas007@users.noreply.github.com>
2024-11-24 15:49:39 -0500

    Cree la clase HistorialDeCompras
    Creación de clase HistorialDeCompras, falta por poner los métodos con implementación de lógica

---
+++ HistorialDeCompras.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class HistorialDeCompras {
+    private Usuario usuario;
+    private List<Boleto> boletos;
+
+    public void registrarBoleto(Boleto boleto) {
+        // Implementación
+    }
+
+    public void consultarBoletos() {
+        // Implementación
+    }
+}
