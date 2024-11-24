0d440848d3c7930122eb5b138c42a40987e53b2d
JUANGM1927 <juan.galeano.martinez@correounivalle.edu.co>
2024-11-24 15:48:46 -0500

    Clase Autenticacion
    Cree la clase autenticacion, falta implementacion

---
+++ Autenticacion.java
@@ -0,0 +1,16 @@
+import java.time.LocalDateTime;
+
+public class Autenticacion {
+    private Usuario usuarioActual;
+    private boolean sesionActiva;
+    private LocalDateTime tiempoExpiracionSesion;
+
+    public boolean iniciarSesion(Usuario usuario, String contrasena) {
+        // Implementación
+        return false; // Temporal
+    }
+
+    public void cerrarSesion() {
+        // Implementación
+    }
+}
