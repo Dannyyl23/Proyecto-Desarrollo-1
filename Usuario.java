e2fe6ef463badfbad2db302f7665159be955ca10
xGelipe <aecheverrig0.3@gmail.com>
2024-11-24 15:21:55 -0500

    Creacion de la clase usuario
    Cree la clase usuario, falta poner las funciones.

---
+++ Usuario.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class Usuario {
+    private String nombreUsuario;
+    private String contrasena;
+    private List<Boleto> historialCompras;
+
+    public void consultarHistorial() {
+        // Implementación
+    }
+}
