803f4a7a1a878f2de3bb0ad9d9d5af66d9bdd4df
Dannyyl23 <berriokevin329@gmail.com>
2024-11-24 15:55:45 -0500

    Clase Sala
    Se crea la clase sala listo para implementar funcionamiento y logica

---
+++ Sala.java
@@ -0,0 +1,14 @@
+public class Sala {
+    private int capacidad;
+    private float asientos;
+    private boolean disponibilidadAsientos;
+
+    public void configurarSala() {
+    }
+
+    public void definirAsientos() {
+    }
+
+    public void definirDisponibilidad() {
+    }
+}
